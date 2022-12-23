fbb919bccdb85cb712876e24832c7dd751ed91cc
Philippe De Pauw - Waterschoot <phildp@MACINTOSH.local>
2022-12-23 10:23:14 +0100

    Added read, srite of posts and users

--- week_05/app/server/api/routes/index.js
+++ week_05/app/server/api/routes/index.js
@@ -5,8 +5,38 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 
 const usersJson = require('../../data/users.json');
+
+/*
+Reading and writing files
+*/
+const filePathUsers = path.resolve(__dirname, '..', '..', 'data', 'users.json');
+
+const readDataFromUsersFile = () => {
+  const data = fs.readFileSync(filePathUsers, { encoding: 'utf-8', flag: 'r+'});
+  const users = JSON.parse(data);
+
+  return users;
+}
+
+const writeDataToUsersFile = (data) => {
+  fs.writeFileSync(filePathUsers, JSON.stringify(data, null, 2));
+}
+
+const filePathPosts = path.resolve(__dirname, '..', '..', 'data', 'posts.json');
+
+const readDataFromPostsFile = () => {
+  const data = fs.readFileSync(filePathPosts, { encoding: 'utf-8', flag: 'r+'});
+  const posts = JSON.parse(data);
+
+  return posts;
+}
+
+const writeDataToPostsFile = (data) => {
+  fs.writeFileSync(filePathPosts, JSON.stringify(data, null, 2));
+}
 
 /*
 Routes for the home end point of the api
@@ -19,48 +49,122 @@ router.get('/', (req, res) => {
 
 // Get all users
 router.get('/users', (req, res) => {
-  res.json(usersJson);
+  const data = readDataFromUsersFile();
+  res.status(200).json(data);
 });
 
 // Get a specific user by id
 router.get('/users/:userId', (req, res) => {
   const { userId } = req.params;
-  const userObj = usersJson.find((obj) => obj.id === userId);
 
-  res.json(userObj);
+  const data = readDataFromUsersFile();
+  const userObj = data.find((obj) => obj.id === userId);
+
+  res.status(200).json(userObj);
 });
 
 // Create a new user
 router.post('/users', (req, res) => {
   const userObj = req.body;
+  userObj.id = uuidv4();
   userObj.createdAt = Date.now();
   userObj.updatedAt = Date.now();
-  console.log(req.body);
+  
+  const data = readDataFromUsersFile();
+  data.push(userObj);
+  writeDataToUsersFile(data);
 
-  res.json(userObj);
+  res.status(200).json(userObj);
 });
 
 // Delete a specific user by id
 router.delete('/users/:userId', (req, res) => {
   const { userId } = req.params;
-  const filteredUsers = usersJson.filter((obj) => obj.id !== userId);
 
-  res.json(filteredUsers);
+  const data = readDataFromUsersFile();
+  const filteredUsers = data.filter((obj) => obj.id !== userId);
+  writeDataToUsersFile(filteredUsers);
+
+  res.status(200).json(filteredUsers);
 });
 
 // Update a specific user by id
 router.put('/users/:userId', (req, res) => {
   const { userId } = req.params;
-  const userIndex = usersJson.findIndex((obj) => obj.id === userId);
+
+  const data = readDataFromUsersFile();
+  const userIndex = data.findIndex((obj) => obj.id === userId);
 
   const userObj = req.body;
-  usersJson[userIndex] = {
-    ...usersJson[userIndex],
-    userObj,
+  data[userIndex] = {
+    ...data[userIndex],
+    ...userObj,
     updatedAt: Date.now()
   }
 
-  res.json(usersJson[userIndex]);
+  writeDataToUsersFile(data);
+
+  res.json(data[userIndex]);
+});
+
+// Get all posts
+router.get('/posts', (req, res) => {
+  const data = readDataFromPostsFile();
+  res.status(200).json(data);
+});
+
+// Get a specific post by id
+router.get('/posts/:postId', (req, res) => {
+  const { postId } = req.params;
+
+  const data = readDataFromPostsFile();
+  const postObj = data.find((obj) => obj.id === postId);
+
+  res.status(200).json(postObj);
+});
+
+// Create a new post
+router.post('/posts', (req, res) => {
+  const postObj = req.body;
+  postObj.id = uuidv4();
+  postObj.createdAt = Date.now();
+  postObj.updatedAt = Date.now();
+  
+  const data = readDataFromPostsFile();
+  data.push(postObj);
+  writeDataToPostsFile(data);
+
+  res.status(200).json(postObj);
+});
+
+// Delete a specific post by id
+router.delete('/posts/:postId', (req, res) => {
+  const { postId } = req.params;
+
+  const data = readDataFromPostsFile();
+  const filteredPosts = data.filter((obj) => obj.id !== postId);
+  writeDataToPostsFile(filteredPosts);
+
+  res.status(200).json(filteredPosts);
+});
+
+// Update a specific post by id
+router.put('/posts/:postId', (req, res) => {
+  const { postId } = req.params;
+
+  const data = readDataFromPostsFile();
+  const postIndex = data.findIndex((obj) => obj.id === postId);
+
+  const postObj = req.body;
+  data[postIndex] = {
+    ...data[postIndex],
+    ...postObj,
+    updatedAt: Date.now()
+  }
+
+  writeDataToPostsFile(data);
+
+  res.status(200).json(data[postIndex]);
 });
 
 // Exports the end points of this route
